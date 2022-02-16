90cc2ad016977cd7e781a0039466903d13ef5da3
JavierPerez21 <javierpereztobia@gmail.com>
2022-02-16 13:14:35 -0800

    Adding new solution

--- Coding_Challenges/qchem_300_Universality_Givens_template/universality_givens.py
+++ Coding_Challenges/qchem_300_Universality_Givens_template/universality_givens.py
@@ -16,35 +16,39 @@ def givens_rotations(a, b, c, d):
     """
 
     # QHACK #
-    # a = cos(X)cos(Z)
-    # b = -sin(X)cos(Y)
-    # c = sin(X)sin(Y)
-    # d = -cos(X)sin(Z)
-    Z = np.arctan(-d / a)
+    #a = cos(X)cos(Z)
+    #b = -sin(X)cos(Y)
+    #c = sin(X)sin(Y)
+    #d = -cos(X)sin(Z)
+    Z = np.arctan(-d/a)
 
-    Y = np.arctan(-c / b)
+    Y = np.arctan(- c / b)
 
     """
-        x = a / np.sin(Y)
-        if abs(x) > 1:
-            x = -b / np.cos(Y)
-        if abs(x) > 1:
-            x =  c / np.sin(Y)
-        if abs(x) > 1:
-            x = -d / np.cos(Y)
-        X = np.arcsin(x)
-        """
+    x = a / np.sin(Y)
+    if abs(x) > 1:
+        x = -b / np.cos(Y)
+    if abs(x) > 1:
+        x =  c / np.sin(Y)
+    if abs(x) > 1:
+        x = -d / np.cos(Y)
+    X = np.arcsin(x)
+    """
     X = np.arctan(-(c * np.sin(Z)) / (d * np.sin(Y)))
 
-    return X * 2, Y * 2, Z * 2
+    return X*2, Y*2, Z*2
 
     # QHACK #
 
 
 if __name__ == "__main__":
     # DO NOT MODIFY anything in this code block
-    inputs = sys.stdin.read().split(",")
+    filepath, answerpath = sys.argv[1], sys.argv[2]
+    with open(filepath, 'r') as f:
+        inputs = f.read().split(",")
     theta_1, theta_2, theta_3 = givens_rotations(
         float(inputs[0]), float(inputs[1]), float(inputs[2]), float(inputs[3])
     )
     print(*[theta_1, theta_2, theta_3], sep=",")
+    with open(answerpath, 'r') as f:
+        print(f.read())
