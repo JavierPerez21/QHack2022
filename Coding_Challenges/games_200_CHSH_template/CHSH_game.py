4c41f22be79b8c7c3111c2fb3b1b15c5504351ec
NolanKoblischke <57974254+NolanKoblischke@users.noreply.github.com>
2022-02-21 14:10:58 -0800

    Commented on CHSH_game.py

--- Coding_Challenges/games_200_CHSH_template/CHSH_game.py
+++ Coding_Challenges/games_200_CHSH_template/CHSH_game.py
@@ -21,9 +21,11 @@ def prepare_entangled(alpha, beta):
     """
     
     # QHACK #
-    #Normalize
+    #Normalize alpha and beta such that |alpha|^2 + |beta|^2 = 1
     alpha_normalized = alpha/np.sqrt(alpha**2 + beta**2)
     beta_normalized = beta/np.sqrt(alpha**2 + beta**2)
+
+    #Create a state psi = alpha |00> + beta |11>
     qml.QubitStateVector(np.array([alpha_normalized, 0, 0, beta_normalized]), wires=[0,1])
     # QHACK #
 
@@ -49,8 +51,17 @@ def chsh_circuit(theta_A0, theta_A1, theta_B0, theta_B1, x, y, alpha, beta):
     
 
     # QHACK #
+
+    # I need to transform |0> and |1> into the basis states |v0> and |v1>
+    # Where |v0> =  cos(theta) |0> + sin(theta) |1>
+    #       |v1> = -sin(theta) |0> + cos(theta) |1>
+    # This can be done by a rotation around the y-axis by theta
+    # Alice chooses theta_A0 when x = 0 and theta_A1 when x = 1
+    # Bob chooses theta_B0 when y = 0 and theta_B1 when y = 1
+
     qml.RY(theta_A0*(1-x) + theta_A1*x, wires=0)
     qml.RY(theta_B0*(1-y) + theta_B1*y, wires=1)
+
     # QHACK #
 
     return qml.probs(wires=[0, 1])
@@ -70,23 +81,33 @@ def winning_prob(params, alpha, beta):
 
     # QHACK #
 
-    # There are four scenarios, (x,y) = (0,0), (0,1), (1,0), (1,1) each with a 1/4 probability
+    # Sometimes pennylane passed params as a AutoGrad object and other times as a numpy array. Weird.
     try:
         params = params._value
     except:
         pass
 
-    #print("Now trying with params:", params)
+    # There are four scenarios, (x,y) = (0,0), (0,1), (1,0), (1,1) each with a 1/4 probability
     prob00 = chsh_circuit(params[0], params[1], params[2], params[3], 0, 0, alpha, beta)
     prob01 = chsh_circuit(params[0], params[1], params[2], params[3], 0, 1, alpha, beta)
     prob10 = chsh_circuit(params[0], params[1], params[2], params[3], 1, 0, alpha, beta)
     prob11 = chsh_circuit(params[0], params[1], params[2], params[3], 1, 1, alpha, beta)
+
     probability_given_xy = np.zeros(4, dtype=float)
+
+    # x*y = 0 when (x,y) = (0,0), (0,1), (1,0)
+    # If x*y = 0, the probability of winning is the probability of a = 0 and b = 0 or a = 1 and b = 1
+    # This is the probability of measuring |00> + probability of measuring |11>
     probability_given_xy[0] = prob00[0] + prob00[3]
     probability_given_xy[1] = prob01[0] + prob01[3]
     probability_given_xy[2] = prob10[0] + prob10[3]
+
+    # x*y = 1 when (x,y) = (1,1)
+    # If x*y = 1, the probability of winning is the probability of a = 0 and b = 1 or a = 1 and b = 0
+    # This is the probability of measuring |01> + probability of measuring |10>
     probability_given_xy[3] = prob11[1] + prob11[2]
-    
+
+    # The total probability of winning is the sum of the probabilities of each scenario * 1/4
     probability_of_win = 0.25*np.sum(probability_given_xy)
 
     return probability_of_win
@@ -125,7 +146,7 @@ def optimize(alpha, beta):
         params = opt.step(cost, params)
         print("Winning Probability:", cost(params))
 
-    #I'll just optimize it myself
+    #I'll just optimize it myself. This produces the right answer. Takes a long time.
     #win_prob = 0
     #best_params = np.array([0.5, 0.5, 0.5, 0.5])
     #for thetaA0 in np.linspace(0, np.pi, steps):
